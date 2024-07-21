ed65844ed331e8e49971fa33f5dc556d6ded88b2
Maximilian118 <maxcrosby118@gmail.com>
2024-07-21 16:13:37 +0200

    more obvious driverGroup comments

--- src/models/driverGroup.ts
+++ src/models/driverGroup.ts
@@ -16,11 +16,11 @@ export interface driverGroupType {
 
 const driverGroupSchema = new mongoose.Schema<driverGroupType>({
   url: { type: String, required: true }, // URL to an image in AWS S3.
-  name: { type: String, required: true }, // Name of the driver championship.
-  championships: [{ type: mongoose.Schema.ObjectId, ref: "Champ" }], // Championships that this driver championship is being used for.
-  drivers: [{ type: mongoose.Schema.ObjectId, ref: "Driver" }], // Drivers that belong in this driver championship.
-  created_at: { type: String, default: moment().format() }, // When the driver championship was created.
-  updated_at: { type: String, default: moment().format() }, // When the driver championship was updated.
+  name: { type: String, required: true }, // Name of the driver group.
+  championships: [{ type: mongoose.Schema.ObjectId, ref: "Champ" }], // Championships that this driver group is being used for.
+  drivers: [{ type: mongoose.Schema.ObjectId, ref: "Driver" }], // Drivers that belong in this driver group.
+  created_at: { type: String, default: moment().format() }, // When the driver group was created.
+  updated_at: { type: String, default: moment().format() }, // When the driver group was updated.
 })
 
 const DriverGroup = mongoose.model<driverGroupType>("DriverGroup", driverGroupSchema)
